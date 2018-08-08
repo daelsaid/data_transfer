c006cb2b10ba6320f6dc9f78129b15d285e72e8f
Dawlat El-Said <daelsaid@stanford.edu>
2018-08-08 11:35:24 -0700

    updated best fmri organization workflow
    fxnns to convert struct, clean up empty dirs, find exams, and actual org

--- best_data_transfer_efile_update.sh
+++ best_data_transfer_efile_update.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
+source ~/.bash_profile
 
 export SPM8DIR="/Applications/spm8_sge"
 export PATH=$PATH:/Volumes/Smurf-Village/SoftwareRepository/etkinlab/Stanford-EtkinLab-bitbucket/analysis_pipeline/
 export PATH=$PATH:/Volumes/Smurf-Village/Imaging/best_ptsd/scripts/fmri
 export PATH=$PATH:/Volumes/Smurf-Village/SoftwareRepository/linux_openmp_64/
+export PATH=$PATH:/Volumes/Smurf-Village/Imaging/best_ptsd/data/mri/new/scripts
 
-main_raw_dir='/Volumes/Smurf-Vault/Scanner_Data_From_Lucas/best_ptsd'
-eeg_fmri_raw_dir='/Volumes/Smurf-Vault/Scanner_Data_From_Lucas/best_ptsd/eeg_fmri/hc'
-fmri_raw_dir='/Volumes/Smurf-Vault/Scanner_Data_From_Lucas/best_ptsd/fmri/patient'
-fmri_raw_hv='/Volumes/Smurf-Vault/Scanner_Data_From_Lucas/best_ptsd/fmri/hv'
-best_data_output_dir='/Volumes/Smurf-Village/Imaging/best_ptsd/data';
+main_raw_dir='/Volumes/Smurf-Vault/Scanner_Data_From_Lucas/best_ptsd';
+eeg_fmri_raw_dir='/Volumes/Smurf-Vault/Scanner_Data_From_Lucas/best_ptsd/eeg_fmri/hc';
+fmri_raw_dir='/Volumes/Smurf-Vault/Scanner_Data_From_Lucas/best_ptsd/fmri/patient';
+fmri_raw_hv='/Volumes/Smurf-Vault/Scanner_Data_From_Lucas/best_ptsd/fmri/hv';
+best_data_output_dir='/Volumes/Smurf-Village/Imaging/best_ptsd/data/new';
 best_village='/Volumes/Smurf-Village/Imaging/best_ptsd';
-fmri_dir='/Volumes/Smurf-Village/Imaging/best_ptsd/data/fmri/best_patient_fmri'
-eeg_fmri_dir='/Volumes/Smurf-Village/Imaging/best_ptsd/data/eegfmri/best_hc_eeg_fmri';
+fmri_dir='/Volumes/Smurf-Village/Imaging/best_ptsd/data/mri/new/';
+eeg_fmri_dir='/Volumes/Smurf-Village/Imaging/best_ptsd/data/mri/new/';
+structs='/Volumes/Smurf-Village/Imaging/best_ptsd/data/mri/new/structs';
+topup='/Volumes/Smurf-Village/Imaging/best_ptsd/data/mri/new/scripts';
 
 cd ${main_raw_dir}
-exams_to_organize=`find ${eeg_fmri_raw_dir}/ ${fmri_raw_dir}/ ${fmri_raw_hv}/ -maxdepth 3 -name 'E?????'`
-echo $exams_to_organize;
+
+function find_exams_to_organize() {
+    cd ${main_raw_dir};
+    exams_to_organize=`find ${eeg_fmri_raw_dir}/ ${fmri_raw_dir}/ ${fmri_raw_hv}/ -maxdepth 3 -name 'E?????'`
+    echo $exams_to_organize;
+}
 
 function empty_dir(){
   if [ "$(ls -A $1)" ]; then
@@ -26,27 +34,66 @@ function empty_dir(){
   fi
 }
 
-for exam in $exams_to_organize; do
-    empty_dir $exam;
-	subj_name=`mri_probedicom --i $exam/anat/001/I0001.dcm | grep PatientName | cut -d' ' -f2`;
-	subj_dir=`readlink -f $(dirname $exam)/${subj_name}`;
-	cd $(dirname $exam);
-	mkdir ${subj_name};
-	mv $exam/* $subj_name;
-	cd ${subj_dir}/anat;
-	for dir in `ls -d *`; do
-		echo $dir;
-		seq_name=`mri_probedicom --i $dir/I0001.dcm | grep SeriesDescription | cut -d' ' -f2`;
-		mkdir ../$seq_name;
-		ionice -c 2 -n 3 cp -uvrp $dir ../${seq_name};
-	done
-	cd ${subj_dir};
-	for efile in `ls E*.*`; do
-		seq_name=` cat $efile | grep "series description" | cut -d' ' -f4`;
-		phys=`echo $efile | cut -b11-16`;
-		mv *$phys* $seq_name;
-	done
-	cd $(dirname $exam);
-	#ApplyTopUp.sh ${subj_name};
-cd ${main_raw_dir};
+#convert structurals fxn
+function best_village_dicom_nii_convert() {
+    dir=$1
+    for subj in `ls -d ${dir}/sag*`;
+    do
+        path=$(dirname $subj)
+        subj_id=`echo $(dirname $subj) | cut -d/ -f7`
+        new_struct_id=${subj_id}_struct.nii.gz
+        summary_with_series_dir=`cat ${path}/summary.txt | grep 184`
+        dcm_dir=`echo $summary_with_series_dir | cut -d' ' -f2 | cut -d: -f1`;
+        echo $dcm_dir
+        cd $subj;
+        echo ${dcm_dir}/I0001.dcm ${structs}${new_struct_id};
+        mri_convert ${dcm_dir}/I0001.dcm ${structs}${new_struct_id};
+        cd ${dir};
+    done
+}
+
+
+function best_raw_data_orgranization() {
+    find_exams_to_organize;
+    for exam in $exams_to_organize; do
+        empty_dir $exam; #checks whether exam folder has data in it, if it doesnt it deletes exam directory d/t assumption that weve already organized
+    	subj_name=`mri_probedicom --i $exam/anat/001/I0001.dcm | grep PatientName | cut -d' ' -f2`; # create the subject folder to move data into based off of the ID we entered at lucas
+    	subj_dir=`readlink -f $(dirname $exam)/${subj_name}`; #subject data path
+    	cd $(dirname $exam); #change directory to main direcotry where exam folder is present
+    	mkdir ${subj_name}; # makes subject folder that we pulled from the dicom header and what the scanning operator entered as the subject ID
+    	mv $exam/* $subj_name; # move all contents from exam folder into the newly created subject folder
+    	cd ${subj_dir}/anat; #change directory into anat and begin the organization
+    	for dir in `ls -d *`; do
+    		echo $dir;
+    		seq_name=`mri_probedicom --i $dir/I0001.dcm | grep SeriesDescription | cut -d' ' -f2`;
+    		mkdir ../$seq_name;
+    		ionice -c 2 -n 3 cp -uvrp $dir ../${seq_name};
+    	done
+    	cd ${subj_dir};
+        #org efiles
+    	for efile in `ls E*.*`; do
+    		seq_name=` cat $efile | grep "series description" | cut -d' ' -f4`;
+    		phys=`echo $efile | cut -b11-16`;
+    		mv *$phys* $seq_name;
+    	done
+        #rename summary and copy tovillage
+        mv SUMMARY summary.txt;
+        cp summary.txt ${best_data_output_dir}/scan_keys/${subj_name}_scan_summary.txt;
+    	cd $(dirname $exam);
+    	ionice -c 2 -n 2 bash ${topup}/ApplyTopUp.sh ${subj_name}; #Apply topup
+        topup_niis=`ls ${subj_dir}/topup/*tu.nii.gz | wc -l` # check if any topups exist
+        if [[ ${topup_niis} -gt 0 ]]; then
+            echo "pa/ap present"
+        else
+            cp ${subj_dir}/topup/*rest*.nii ${best_data_output_dir}/rest;
+            cp ${subj_dir}/topup/*colorid.nii ${best_data_output_dir}/colorid;
+        fi #copy the .nii of scans if no topup to best output dir
+    best_village_dicom_nii_convert ${subj_dir} #convert structs 
+    cd ${main_raw_dir};
 done &
+}
+
+
+
+
+
